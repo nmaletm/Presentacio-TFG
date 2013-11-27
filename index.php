1b48da8c8c8d3dd3d897ba54dcb763742293e6b2
Nestor Malet <nmaletm@gmail.com>
2013-11-27 22:36:02 +0100

    Estil basic

---
+++ index.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+
+		<title>StreamUPC - Presentació</title>
+
+		<meta name="description" content="Presentació TFG">
+		<meta name="author" content="Néstor Malet">
+
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		<link rel="stylesheet" href="css/reveal.min.css">
+		<link rel="stylesheet" href="css/theme/solarized.css" id="theme">
+
+		<!-- For syntax highlighting -->
+		<link rel="stylesheet" href="lib/css/zenburn.css">
+
+		<link rel="stylesheet" href="css/custom.css">
+
+		<!-- If the query includes 'print-pdf', use the PDF print sheet -->
+		<script>
+			document.write( '<link rel="stylesheet" href="css/print/' + ( window.location.search.match( /print-pdf/gi ) ? 'pdf' : 'paper' ) + '.css" type="text/css" media="print">' );
+		</script>
+
+		<!--[if lt IE 9]>
+		<script src="lib/js/html5shiv.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+
+		<div class="reveal reveal-viewport">
+
+<div class="state-background"><div class="logo"></div></div>
+<!-- inici slides -->
+<div class="slides">
+    <section>
+        <h1>StreamUPC <br>Un nou concepte de col·laboració<br>
+        </h1>
+    </section>
+    <section>
+        <h2>Metodologia</h2>
+		<ul>
+			<li></li>
+		</ul>
+    </section>
+    <section>
+        <h2>Pressupost</h2>
+		<table>
+			<tr>
+				<td>Col</td>
+				<td>Col</td>
+			</tr>
+			<tr>
+				<td>Col</td>
+				<td>Col</td>
+			</tr>
+		</table>
+    </section>
+
+    <section>
+        <h2>Planificació</h2>
+		
+    </section>
+
+    <section>
+        <h2>Demostració</h2>
+		<video class="video-demo" src="demo.mov"></video>
+    </section>
+
+    <section>
+        <h2>Conclusions</h2>
+		
+    </section>
+</div>
+
+		<!-- final slides -->
+		</div>
+
+		<script src="lib/js/head.min.js"></script>
+		<script src="js/reveal.min.js"></script>
+
+		<script>
+
+			// Full list of configuration options available here:
+			// https://github.com/hakimel/reveal.js#configuration
+			Reveal.initialize({
+				controls: true,
+				progress: true,
+				history: true,
+				center: true,
+
+				theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
+				transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
+
+				// Optional libraries used to extend on reveal.js
+				dependencies: [
+					{ src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+					{ src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+					{ src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+					{ src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+					{ src: 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+					{ src: 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+				]
+			});
+
+		</script>
+
+	</body>
+</html>
